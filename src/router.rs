d4e20e072604122148dc424fce33207ac36e981c
Roberto Orellana <rorepoid@gmail.com>
2026-01-24 18:08:22 -0500

    Refactor: Move router definition to its own module
    The `Route` enum and its associated routing logic have been moved from
    `main.rs` into a new `router.rs` module. This improves organization and
    separation of concerns, making the codebase more maintainable.

---
+++ src/router.rs
@@ -0,0 +1,19 @@
+use dioxus::prelude::*;
+
+use crate::views::{Blog, Home, Navbar};
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+pub enum Route {
+    // The layout attribute defines a wrapper for all routes under the layout. Layouts are great for wrapping
+    // many routes with a common UI like a navbar.
+    #[layout(Navbar)]
+        #[route("/")]
+        Home {},
+        // The route attribute can include dynamic parameters that implement [`std::str::FromStr`] and [`std::fmt::Display`] with the `:` syntax.
+        // In this case, id will match any integer like `/blog/123` or `/blog/-456`.
+        #[route("/blog/:id")]
+        // Fields of the route variant will be passed to the component as props. In this case, the blog component must accept
+        // an `id` prop of type `i32`.
+        Blog { id: i32 },
+}
