41d9911bb722c016e99da96e0bb24ef97f150625
Roberto Orellana <rorepoid@gmail.com>
2026-01-24 22:15:39 -0500

    Remove redundant navbar documentation
    The documentation for the `Navbar` component was redundant and has been
    removed. The component's purpose is clear from its name and usage within
    the routing setup.

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -3,11 +3,6 @@ use dioxus::prelude::*;
 
 const CSS: Asset = asset!("/assets/styling/navbar.css");
 
-/// The Navbar component that will be rendered on all pages of our app since every page is under the layout.
-///
-///
-/// This layout component wraps the UI of [Route::Home] and [Route::Blog] in a common navbar. The contents of the Home and Blog
-/// routes will be rendered under the outlet inside this component
 #[component]
 pub fn Navbar() -> Element {
     rsx! {
